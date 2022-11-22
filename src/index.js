72930a3b6bc38ef074a177b45e861cb7ec87e2da
Irina <iargina.irina@gmail.com>
2022-11-23 01:03:07 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,71 +1,62 @@
 import Notiflix from '../node_modules/notiflix';
-import axios from 'axios';
 import { AxiosGet } from './axionGet';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import template from './templates/gallery.hbs';
 
 const galleryDiv = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 const loadBtn = document.querySelector('.load-more');
-const getting = new AxiosGet();
+const gettingRequest = new AxiosGet();
+let gallery = new SimpleLightbox('.photo-card a');
+gallery.on('show.simplelightbox', function () {});
 
+form.addEventListener('submit', galleryCardsCreation);
+loadBtn.addEventListener('click', galleryCardsLoading);
 
-
-
-
-form.addEventListener('submit', divCreate);
-loadBtn.addEventListener('click', newCreations);
-
-function newCreations(event) {
+function galleryCardsLoading(event) {
   event.preventDefault();
-  getting.page +=1
-  console.log(getting.page); 
+  gettingRequest.page += 1;
   gettingPhotos();
 }
 
-function divCreate(event) {
+function galleryCardsCreation(event) {
   event.preventDefault();
   if (!event.target.firstElementChild.value) {
+    loadBtn.hidden = true;
+    galleryDiv.innerHTML = '';
     Notiflix.Notify.warning(
       'You did not enter a search query. Please try again.'
     );
-    return (galleryDiv.innerHTML = '');
+    return;
   }
-
-  getting.q = event.target.firstElementChild.value;
-  getting.page = 1;
-
+  event.preventDefault();
+  gettingRequest.q = event.target.firstElementChild.value;
+  gettingRequest.page = 1;
+  galleryDiv.innerHTML = '';
   gettingPhotos();
-
 }
 
-
-
 function gettingPhotos() {
-  getting
+  gettingRequest
     .getPhoto()
-    .then(function (response) {     
-        const images = response.data.hits;
+    .then(function (response) {
+      const images = response.data.hits;
       loadBtn.hidden = false;
       markupGalary(images);
-      const { height: cardHeight } = galleryDiv
-      .firstElementChild.getBoundingClientRect();
-    
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: "smooth",
-    });
- let gallery = new SimpleLightbox('.photo-card a');
+      const { height: cardHeight } =
+        galleryDiv.firstElementChild.getBoundingClientRect();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
 
-      gallery.on('show.simplelightbox', function () {
-      }).refresh();
-
-      if ((getting.page === 1)) {
+      if (gettingRequest.page === 1) {
         Notiflix.Notify.success(
           `Hooray! We found ${response.data.totalHits} images.`
         );
       }
-      if (Math.ceil(response.data.total / 40) === getting.page) {
+      if (Math.ceil(response.data.total / 40) === gettingRequest.page) {
         loadBtn.hidden = true;
         Notiflix.Notify.failure(
           "We're sorry, but you've reached the end of search results."
@@ -77,7 +68,6 @@ function gettingPhotos() {
     });
 }
 
-
 function markupGalary(images) {
   if (images.length < 1) {
     Notiflix.Notify.warning(
@@ -86,38 +76,9 @@ function markupGalary(images) {
     loadBtn.hidden = true;
     return;
   }
-  const item = images.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `
-      <div class="photo-card">
-      <a href="${largeImageURL}"> <img src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b> ${likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b> ${views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b> ${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b> ${downloads}
-          </p>
-        </div>
-      </div>
-            `;
-    }
-  );
 
-  galleryDiv.insertAdjacentHTML('beforeend', item.join(''))
+  const item = template(images);
+  galleryDiv.insertAdjacentHTML('beforeend', item);
+  gallery.refresh();
   return;
 }
